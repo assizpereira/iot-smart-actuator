86bd7ae5e17fd90ff1e55626cad499c0381d2d53
Assiz Pereira <assiz.pereira@bbc.co.uk>
2024-10-09 19:12:19 +0100

    setup toggle function

--- controller-payload.ino
+++ controller-payload.ino
@@ -9,3 +9,24 @@ Servo servo;
 
 
 #define RelayPin1 14  //D1
+
+
+void toggleActuator(uint8_t brightness)
+{
+// Simulate press in
+  if (brightness == 255)
+    {
+
+      servo.write(0);
+      delay(1000);
+      
+      Serial.println("Switch ON");
+    }
+  else
+  {
+    servo.write(100);
+    delay(1000);
+    servo.write(0);
+    Serial.println("Switch OFF");
+  }
+}
